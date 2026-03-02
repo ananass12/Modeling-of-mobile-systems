d70e967f166a96d29510589796f313cdbbe71cf1
Anastasia <pomelova-2004@mail.ru>
2026-03-02 14:40:47 +0700

    правки в 4 лабе и сделана 5 лаба

--- practice4.m
+++ practice4.m
@@ -17,7 +17,7 @@ c = [];
 switch type
     case 1 % QPSK
         for i = 1:2:length(bits)
-            complex = 0.5 * ((1-2*bits(i)) + 1j*(1-2*bits(i+1)));
+            complex = (1/sqrt(2))* * ((1-2*bits(i)) + 1j*(1-2*bits(i+1)));
             fprintf('(%g%+gi) ', real(complex), imag(complex));
             c = [c, complex];
         end
@@ -69,9 +69,9 @@ switch type
         for i = 1:length(c)
             r = real(c(i)) * sqrt(10);
             im = imag(c(i)) * sqrt(10);
-            bit1 = double(r < 0);
+            bit1 = double(r < 0);   % знаковые биты
             bit2 = double(im < 0);
-            bit3 = double(abs(r) > 2);  
+            bit3 = double(abs(r) > 2);   % амплитуда (1 и 3 - граница по 2)
             bit4 = double(abs(im) > 2);
             bits_demod = [bits_demod, bit1, bit2, bit3, bit4];
             fprintf('%d%d%d%d ', bit1, bit2, bit3, bit4);
@@ -81,14 +81,14 @@ switch type
         for i = 1:length(c)
             r = real(c(i)) * sqrt(42);
             im = imag(c(i)) * sqrt(42);
-            bit1 = double(r < 0);
-            bit2 = double(im < 0);
-            amp_r = round(abs(r));
-            amp_im = round(abs(im));
-            bit3 = double(amp_r > 4);          
-            bit4 = double(amp_im > 4);
-            bit5 = double(mod(amp_r, 4) == 3); 
-            bit6 = double(mod(amp_im, 4) == 3);
+            bit1 = double(r < 0);  % знаки 
+            bit2 = double(im < 0); 
+            amp_r = round(abs(r));  % амплитуда 1 3 5 7
+            amp_im = round(abs(im));  
+            bit3 = double(amp_r > 4);      % разделяем значения 1 3 и 5 7
+            bit4 = double(amp_im > 4);     
+            bit5 = double(mod(amp_r, 4) == 3);  % если 1 или 5 - 0, если 3 или 7 - 1
+            bit6 = double(mod(amp_im, 4) == 3); 
             bits_demod = [bits_demod, bit1, bit2, bit3, bit4, bit5, bit6];
             fprintf('%d%d%d%d%d%d ', bit1, bit2, bit3, bit4, bit5, bit6);
         end
@@ -97,15 +97,15 @@ switch type
         for i = 1:length(c)
             r = real(c(i)) * sqrt(170);
             im = imag(c(i)) * sqrt(170);
-            bit1 = double(r < 0);
+            bit1 = double(r < 0);  % определяем квадрант
             bit2 = double(im < 0);
             amp_r = round(abs(r));
             amp_im = round(abs(im));
-            bit3 = double(amp_r > 8);           
+            bit3 = double(amp_r > 8);      % делим на группы относительно 8     
             bit4 = double(amp_im > 8);
-            bit5 = double(mod(amp_r, 8) >= 4);  
+            bit5 = double(mod(amp_r, 8) >= 4);  % выделяем значения 5,7 (в нижней группе) и 13,15 (в верхней)
             bit6 = double(mod(amp_im, 8) >= 4);
-            bit7 = double(mod(amp_r, 4) >= 2);  
+            bit7 = double(mod(amp_r, 4) >= 2);   %  1 для 3, 7, 11, 15     0 для 1, 5, 9, 13
             bit8 = double(mod(amp_im, 4) >= 2);
             bits_demod = [bits_demod, bit1, bit2, bit3, bit4, bit5, bit6, bit7, bit8];
             fprintf('%d%d%d%d%d%d%d%d ', bit1, bit2, bit3, bit4, bit5, bit6, bit7, bit8);
